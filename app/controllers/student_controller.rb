6d36d657ab094bc363c329f06f5204dde8e055fe
Ryan Pethel <arpethel@gmail.com>
2019-07-27 21:48:45 +0000

    Automatically backed up by Learn

--- app/controllers/student_controller.rb
+++ app/controllers/student_controller.rb
@@ -1,2 +1,36 @@
 class StudentController < ApplicationController
+  def index
+		@students = Student.all
+	end
+
+	def show
+		@student = Student.find(params[:id])
+	end
+
+	def new
+		@student = Student.new
+	end
+
+	def create
+		@student = Student.new(student_params(:first_name, :last_name))
+	  @student.save
+	  redirect_to student_path(@student)
+	end
+
+	def update
+	  @student = Student.find(params[:id])
+		@student.update(student_params(:first_name))
+    @student.update(student_params(:last_name))
+	  redirect_to student_path(@student)
+	end
+
+	def edit
+	  @student = Student.find(params[:id])
+	end
+
+	private
+
+	def student_params(*args)
+		params.require(:student).permit(*args)
+	end
 end
